4789e340f31724ef9242b0fdff216c7ae6c90a76
Rebecca Spence <Bec@Rebeccas-MacBook-Pro.local>
2019-11-26 14:36:30 +0000

    fixed navbar /only active users can see the pages

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   const userId = req.session.user;
-    if (userId) {
+    if (userId && req.user.status === "Active" ) {
       next();
     } else {
       next(new Error('User has no permission to visit that page.'));
